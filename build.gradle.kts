cbb5213b7a0a37321dc511b807de594a0d9ba616
isXander <xandersmith2008@gmail.com>
2021-11-06 09:47:33 +0000

    switch to kotlin gradle

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.31"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    application
+}
+
+group = "org.bundleproject"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-client-gson:1.6.5")
+    implementation("io.ktor:ktor-client-core:1.6.5")
+    implementation("io.ktor:ktor-client-apache:1.6.5")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.31")
+
+    implementation("com.formdev:flatlaf:1.6.1")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("com.xenomachina:kotlin-argparser:2.0.7") {
+        exclude(module = "kotlin-stdlib")
+    }
+}
+
+application {
+    mainClass.set("org.bundleproject.installer.MainKt")
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+}
